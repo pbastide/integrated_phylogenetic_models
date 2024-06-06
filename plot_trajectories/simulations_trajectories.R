44c057ee405569a28db716c8084d0b4afac022fb
Paul Bastide <paul.bastide@m4x.org>
2024-06-06 10:10:24 +0200

    Add comments to trajectories

--- plot_trajectories/simulations_trajectories.R
+++ plot_trajectories/simulations_trajectories.R
@@ -1,3 +1,27 @@
+# Copyright (C) {2024} {PB, SG}
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+################################################################################
+## Content:
+#
+# Simulation and plot of trajectories of the BM, OU, IBM and IOU process
+# on a simple tree.
+#
+################################################################################
+
 library(ape)
 library(ggplot2)
 library(cowplot)
