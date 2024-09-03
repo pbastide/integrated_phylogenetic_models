01f0eacf715c4a2037f70f67bb6901fe201d53bb
Stephane Guindon <stephane.guindon@gmail.com>
2024-09-03 14:12:53 +0200

    Fixed issues with multi_phyrex.py

--- wnv_prediction/multi_phyrex.py
+++ wnv_prediction/multi_phyrex.py
@@ -11,19 +11,19 @@ def scan_and_extract(root_dir):
 
     for dirpath, dirnames, filenames in os.walk(root_dir):
         for filename in filenames:            
-            if filename.endswith('.xml') and filename.find("config") != -1:
-                file_path = os.path.join(dirpath, filename)
+            if filename.endswith('.xml') and filename.find("config") and not (filename.find("_rrw") < 0 and filename.find("_ibm") < 0):
+                file_path = os.path.abspath(os.path.join(dirpath, filename))
                 list_of_config_files.append(file_path)
-                print(f"Found {file_path}")   
+                print(f"Found {file_path} {filename} in directory {dirpath}")   
 
     return(list_of_config_files)
 
 def run_phyrex(list_of_config_files):
 
     for config_file in list_of_config_files:
-        print(f"Running phyrex on {config_file} in directory {os.path.dirname(config_file)}")
-        os.chdir(os.path.dirname(config_file))
-        os.system(f"../../phyrex_install/phyrex --xml={os.path.basename(config_file)}")
+        print(f"Running phyrex on {config_file} in directory {config_file.rsplit('/',1)[0]}")
+        os.chdir(config_file.rsplit('/',1)[0])
+        os.system(f"../../phyrex_install/phyrex --xml={config_file}")
 
 
 print(f"Working directory: {os.getcwd()}")
